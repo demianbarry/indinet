6e1144b86813e061ec215b45cbb08a3e33044a6f
demian.barry@gmail.com <demian.barry@gmail.com@5cfd97fb-bc67-63fa-f7a8-54c6675ad5f2>
2013-06-26 02:05:06 +0000

    emprolijamiento del login como controller e incorporación del modelo de Account

--- trunk/proto1/app/controllers/accounts_controller.js
+++ trunk/proto1/app/controllers/accounts_controller.js
@@ -1,5 +1,4 @@
-var v1 = '/api/v1',
-        utils = require('../../lib/utils'),
+var utils = require('../../lib/utils'),
         _ = require('underscore'),
         NotFound = utils.NotFound,
         checkErr = utils.checkErr,
@@ -7,9 +6,26 @@ var v1 = '/api/v1',
         AccountsController;
 
 AccountsController = function(app, mongoose, config) {
-    app.get(v1 + '/login', function(req, res, next) {
+    app.post('/login', function(req, res) {
         console.log('login request');
-        res.end('pepe');
+        var username = req.param('username', null);
+        var password = req.param('password', null);
+
+        if (null === username || username.length < 1
+                || null === password || password.length < 1) {
+            res.send(400);
+            return;
+        }
+
+        Account.login(email, password, function(success) {
+            if (!success) {
+                res.send(401);
+                return;
+            }
+            console.log('login was successful');
+            req.session.loggedIn = true;
+            res.send(200);
+        });
     });
 };
 
